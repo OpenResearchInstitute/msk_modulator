59da82ffaae66d7befcb6b48a50aea9322892a84
Matthew Wishek <matthew@wishek.com>
2024-05-06 13:56:01 -0600

    Switch modulator to Massey design with sequence transducer (i.e. convolutional encoder)

--- msk_modulator.vhd
+++ msk_modulator.vhd
@@ -16,12 +16,13 @@ ENTITY msk_modulator IS
 		clk 				: IN  std_logic;
 		init 				: IN  std_logic;
 
-		tclk 				: OUT std_logic;
+		tclk_out			: OUT std_logic;
 
-		mod_freq_word 		: IN  std_logic_vector(NCO_W -1 DOWNTO 0);
-		car_freq_word	 	: IN  std_logic_vector(NCO_W -1 DOWNTO 0);
+		freq_word_tclk 		: IN  std_logic_vector(NCO_W -1 DOWNTO 0);
+		freq_word_f1 		: IN  std_logic_vector(NCO_W -1 DOWNTO 0);
+		freq_word_f2	 	: IN  std_logic_vector(NCO_W -1 DOWNTO 0);
 
-		tx_data 			: IN  std_logic_vector(1 DOWNTO 0);
+		tx_data 			: IN  std_logic;
 		tx_req 				: OUT std_logic;
 
 		tx_samples	 		: OUT std_logic_vector(SAMPLE_W -1 DOWNTO 0)
@@ -30,171 +31,111 @@ END ENTITY msk_modulator;
 
 ARCHITECTURE rtl OF msk_modulator IS 
 
-	SIGNAL mod_phase 		: std_logic_vector(NCO_W -1 DOWNTO 0);
-	SIGNAL rollover_pi2 	: std_logic;
-	SIGNAL rollover_pi 		: std_logic;
-	SIGNAL rollover_3pi2 	: std_logic;
-	SIGNAL rollover_2pi 	: std_logic;
 	SIGNAL tclk_even		: std_logic;
 	SIGNAL tclk_odd 		: std_logic;
 
-	SIGNAL car_phase 		: std_logic_vector(NCO_W -1 DOWNTO 0);
+	SIGNAL tclk 			: std_logic;
 
-	SIGNAL mod_sin	 		: std_logic_vector(SINUSOID_W -1 DOWNTO 0);
-	SIGNAL mod_cos	 		: std_logic_vector(SINUSOID_W -1 DOWNTO 0);
-	SIGNAL mod_sin_d 		: std_logic_vector(SINUSOID_W -1 DOWNTO 0);
-	SIGNAL mod_cos_d 		: std_logic_vector(SINUSOID_W -1 DOWNTO 0);
+	SIGNAL carrier_phase_f1	: std_logic_vector(NCO_W -1 DOWNTO 0);
+	SIGNAL carrier_phase_f2	: std_logic_vector(NCO_W -1 DOWNTO 0);
+	SIGNAL carrier_cos_f1	: std_logic_vector(SINUSOID_W -1 DOWNTO 0);
+	SIGNAL carrier_cos_f2	: std_logic_vector(SINUSOID_W -1 DOWNTO 0);
 
-	SIGNAL carrier_sin	 	: std_logic_vector(SINUSOID_W -1 DOWNTO 0);
-	SIGNAL carrier_cos	 	: std_logic_vector(SINUSOID_W -1 DOWNTO 0);
+	SIGNAL tclk_dly 		: std_logic_vector(0 TO 3);
 
-	SIGNAL tx_req_d 		: std_logic;
+	SIGNAL tx_data_reg		: std_logic;
 
-	SIGNAL tx_data_reg		: std_logic_vector(1 DOWNTO 0);
+	SIGNAL d_val, d_dly 	: signed(2 DOWNTO 0);
+	SIGNAL d_pos, d_neg 	: signed(1 DOWNTO 0);
+	SIGNAL b_val 			: signed(1 DOWNTO 0);
+	SIGNAL d_n_b 			: signed(1 DOWNTO 0);
+	SIGNAL d_s1, d_s2 		: signed(1 DOWNTO 0);
 
-	SIGNAL tx_enc_even		: std_logic;
-	SIGNAL tx_enc_odd 		: std_logic;
+	SIGNAL s1, s2 			: signed(SINUSOID_W -1 DOWNTO 0);
 
-	SIGNAL tx_enc_odd_delay	: std_logic;
-
-	SIGNAL tx_data_enc		: std_logic_vector(1 DOWNTO 0);
-
-	SIGNAL tx_symbol_I		: std_logic_vector(SINUSOID_W -1 DOWNTO 0);
-	SIGNAL tx_symbol_Q		: std_logic_vector(SINUSOID_W -1 DOWNTO 0);
-
-	SIGNAL tx_sample_I 		: signed(2*SINUSOID_W -1 DOWNTO 0);
-	SIGNAL tx_sample_Q 		: signed(2*SINUSOID_W -1 DOWNTO 0);
-
-	--TYPE diff_enc_lut_type IS ARRAY(0 TO 7) OF std_logic_vector(1 DOWNTO 0);
-
-	--CONSTANT diff_enc_lut 	: diff_enc_lut_type := (
-	--								0 => "0100",			--  1  1  1 =>  1  0
-	--								1 => "0001",			--  1  1 -1 =>  0  1
-	--								2 => "0011",			--  1 -1  1 =>  0 -1
-	--								3 => "1100",			--  1 -1 -1 => -1  0
-	--								4 => "0100",			-- -1  1  1 =>  1  0
-	--								5 => "0011",			-- -1  1 -1 =>  0 -1
-	--								6 => "0001",			-- -1 -1  1 =>  0  1
-	--								7 => "1100" ); 			-- -1 -1 -1 => -1  0
+	SIGNAL b_n 				: std_logic;
 
 BEGIN
 
-	tx_req 	<= tclk_even;
-
-	tclk 	<= tclk_even OR tclk_odd;
+	tclk 	 	<= tclk_even OR tclk_odd;
+	tx_req 		<= tclk;
+	tclk_out 	<= tclk;
 
 	get_data_proc : PROCESS (clk)
 	BEGIN
 		IF clk'EVENT AND clk = '1' THEN
 
-			tx_req_d <= tclk_even;
+			tclk_dly <= tclk & tclk_dly(0 TO 2);
 
-			IF tx_req_d = '1' THEN
+			IF tclk_dly(0) = '1' THEN
 				tx_data_reg	<= tx_data;
 			END IF;
 
 			IF init = '1' THEN 
-				tx_req_d 	<= '0';
-				tx_data_reg	<= "00";
+				tclk_dly 	<= (OTHERS => '0');
+				tx_data_reg	<= '0';
 			END IF;
 
 		END IF;
 	END PROCESS get_data_proc;
 
-	-- Differential Encoder
-	--
-	-- See Section 5.8 Alternative scheme–differential encoder of 
-	-- 		_Wireless Communications: Principles, Theory and Methodology_, Zhang
-	--
-	--	β_n = ⍺_n * β_(n-1) where ⍺_n is the input data sequence and ⍺ ∈ {-1,1}
-	--
-	--	A single-bit number can be treated as signed where:
-	--	
-	--	1 -> -1
-	--  0 ->  1
-	--
-	-- tx_enc_even <= β_(2n)
-	-- tx_enc_odd  <= β_(2n+1)
-	--
+	d_val <= "001" WHEN tx_data_reg = '0' ELSE "111";  -- 0 -> 1 and 1 -> -1
 
-	diff_enc_proc : PROCESS (clk)
+	d_pos <= resize(shift_right(d_val + d_dly, 1), 2);
+	d_neg <= resize(shift_right(d_val - d_dly, 1), 2);
+
+	b_val <= "01" WHEN b_n = '0' ELSE "11";
+
+	d_n_b <= resize(d_neg * b_val, 2);
+
+	enc_proc : PROCESS (clk)
 	BEGIN
 		IF clk'EVENT AND clk = '1' THEN
 
-			IF tclk_even = '1' THEN		-- only process on symbol boundaries
+			IF tclk_dly(0) = '1' THEN 
 
-				tx_enc_even  	<= tx_data_reg(0) XOR tx_enc_odd;
-				tx_enc_odd  	<= tx_data_reg(1) XOR tx_data_reg(0) XOR tx_enc_odd;
+				b_n 	<= NOT b_n; 						-- b[n] = (-1)^n
+				d_dly 	<= d_val;
 
 			END IF;
 
-			IF init = '1' THEN 
-				tx_enc_even 	<= '1';
-				tx_enc_odd  	<= '1';
+			IF tclk_dly(1) = '1' THEN
+
+				d_s1 	<= d_pos;
+				d_s2 	<= d_n_b;
+
 			END IF;
-
-		END IF;
-	END PROCESS diff_enc_proc;
-
-	tx_data_enc <= tx_enc_odd_delay & tx_enc_even;
-
-	shape_proc : PROCESS (clk)
-	BEGIN
-		IF clk'EVENT AND clk = '1' THEN
-
-			-- Delay sin/cos by 1 clock to align with tx_I/Q
-			mod_sin_d <= mod_sin;				
-			mod_cos_d <= mod_cos;
-
-			-- Offset odd channel by 1/2 symbol period (T)
-			IF tclk_odd = '1' THEN
-				tx_enc_odd_delay <= tx_enc_odd;
-			END IF;
-
-			-- Tx_I/Tx_Q are a single bit, but treated as signed. 0 => 1, 1 => -1
-
-			-- The multiplies are implemented as muxes because we are multiplying by
-			-- either 1 or -1. Perhaps the toolchain will optimize to muxes, but
-			-- we code as muxes to ensure the best result.
-
-			-- tx_symbol_I <= tx_I * mod_cos
-			IF tx_enc_even = '1' THEN
-				tx_symbol_I <= std_logic_vector(unsigned(NOT(mod_cos_d)) + 1);		-- multiply by -1
-			ELSE
-				tx_symbol_I <= mod_cos_d;
-			END IF;
-
-			-- tx_symbol_Q <= tx_Q * mod_sin
-			IF tx_enc_odd_delay = '1' THEN
-				tx_symbol_Q <= std_logic_vector(unsigned(NOT(mod_sin_d)) + 1); 		-- multiply by -1
-			ELSE
-				tx_symbol_Q <= mod_sin_d;
-			END IF;
-
-		END IF;
-	END PROCESS shape_proc;
-
-
-	mod_proc : PROCESS (clk)
-	BEGIN
-		IF clk'EVENT AND clk = '1' THEN
-
-			tx_sample_I <= signed(tx_symbol_I) * signed(carrier_cos);
-			tx_sample_Q <= signed(tx_symbol_Q) * signed(carrier_sin);
-
-			tx_samples  <= std_logic_vector(resize(shift_right(tx_sample_I + tx_sample_Q, SINUSOID_W), SAMPLE_W));
 
 			IF init = '1' THEN
-				tx_samples  <= (OTHERS => '0');
-				tx_sample_I <= (OTHERS => '0');
-				tx_sample_Q <= (OTHERS => '0');
+				b_n 	<= '0';
+				d_dly 	<= "001";
+				d_s1 	<= "00";
+				d_s2	<= "00";
 			END IF;
 
 		END IF;
-	END PROCESS mod_proc;
+	END PROCESS enc_proc;
 
 
-	U_mod_nco : ENTITY work.nco(rtl)
+	carrier_mod_proc : PROCESS (clk)
+	BEGIN
+		IF clk'EVENT AND clk = '1' THEN
+
+			s1 <= resize(d_s1 * signed(carrier_cos_f1), SINUSOID_W);
+			s2 <= resize(d_s2 * signed(carrier_cos_f2), SINUSOID_W);
+
+			tx_samples <= std_logic_vector(s1 + s2);
+
+			IF init = '1' THEN
+				s1 <= (OTHERS => '0');
+				s2 <= (OTHERS => '0');
+				tx_samples <= (OTHERS => '0');
+			END IF;
+		END IF;
+	END PROCESS carrier_mod_proc;
+
+
+	U_tclk_nco : ENTITY work.nco(rtl)
 	GENERIC MAP(
 		NCO_W 			=> NCO_W
 	)
@@ -202,36 +143,19 @@ BEGIN
 		clk 			=> clk,
 		init 			=> init,
 	
-		freq_word 		=> mod_freq_word,
+		freq_word 		=> freq_word_tclk,
 		freq_adjust 	=> std_logic_vector(to_signed(0, NCO_W)),
 	
-		phase    		=> mod_phase,
-		rollover_pi2 	=> rollover_pi2,
-		rollover_pi 	=> rollover_pi,
-		rollover_3pi2 	=> rollover_3pi2,
-		rollover_2pi 	=> rollover_2pi,
+		phase    		=> OPEN,
+		rollover_pi2 	=> OPEN,
+		rollover_pi 	=> OPEN,
+		rollover_3pi2 	=> OPEN,
+		rollover_2pi 	=> OPEN,
 		tclk_even		=> tclk_even,
 		tclk_odd		=> tclk_odd
 	);
 
-	U_mod_sin_cos_lut : ENTITY work.sin_cos_lut(lut_based)
-	GENERIC MAP(
-		PHASE_W 		=> PHASE_W,
-		PHASES 			=> 2**PHASE_W,
-		SINUSOID_W 		=> SINUSOID_W
-	)
-	PORT MAP(
-		clk 			=> clk,
-		init 			=> init,
-	
-		phase 			=> mod_phase(NCO_W -1 DOWNTO NCO_W - PHASE_W),
-
-		sin_out			=> mod_sin,
-		cos_out			=> mod_cos
-	);
-
-
-	U_carrier_nco : ENTITY work.nco(rtl)
+	U_f1_nco : ENTITY work.nco(rtl)
 	GENERIC MAP(
 		NCO_W 			=> NCO_W
 	)
@@ -239,10 +163,10 @@ BEGIN
 		clk 			=> clk,
 		init 			=> init,
 	
-		freq_word 		=> car_freq_word,
+		freq_word 		=> freq_word_f1,
 		freq_adjust 	=> std_logic_vector(to_signed(0, NCO_W)),
 	
-		phase    		=> car_phase,
+		phase    		=> carrier_phase_f1,
 		rollover_pi2 	=> OPEN,
 		rollover_pi 	=> OPEN,
 		rollover_3pi2 	=> OPEN,
@@ -251,7 +175,7 @@ BEGIN
 		tclk_odd		=> OPEN
 	);
 
-	U_carrier_sin_cos_lut : ENTITY work.sin_cos_lut(lut_based)
+	U_f1_sin_cos_lut : ENTITY work.sin_cos_lut(lut_based)
 	GENERIC MAP(
 		PHASE_W 		=> PHASE_W,
 		PHASES 			=> 2**PHASE_W,
@@ -261,10 +185,46 @@ BEGIN
 		clk 			=> clk,
 		init 			=> init,
 	
-		phase 			=> car_phase(NCO_W -1 DOWNTO NCO_W - PHASE_W),
+		phase 			=> carrier_phase_f1(NCO_W -1 DOWNTO NCO_W - PHASE_W),
 
-		sin_out			=> carrier_sin,
-		cos_out			=> carrier_cos
+		sin_out			=> OPEN,
+		cos_out			=> carrier_cos_f1
+	);
+
+	U_f2_nco : ENTITY work.nco(rtl)
+	GENERIC MAP(
+		NCO_W 			=> NCO_W
+	)
+	PORT MAP(
+		clk 			=> clk,
+		init 			=> init,
+	
+		freq_word 		=> freq_word_f2,
+		freq_adjust 	=> std_logic_vector(to_signed(0, NCO_W)),
+	
+		phase    		=> carrier_phase_f2,
+		rollover_pi2 	=> OPEN,
+		rollover_pi 	=> OPEN,
+		rollover_3pi2 	=> OPEN,
+		rollover_2pi 	=> OPEN,
+		tclk_even		=> OPEN,
+		tclk_odd		=> OPEN
+	);
+
+	U_f2_sin_cos_lut : ENTITY work.sin_cos_lut(lut_based)
+	GENERIC MAP(
+		PHASE_W 		=> PHASE_W,
+		PHASES 			=> 2**PHASE_W,
+		SINUSOID_W 		=> SINUSOID_W
+	)
+	PORT MAP(
+		clk 			=> clk,
+		init 			=> init,
+	
+		phase 			=> carrier_phase_f2(NCO_W -1 DOWNTO NCO_W - PHASE_W),
+
+		sin_out			=> OPEN,
+		cos_out			=> carrier_cos_f2
 	);
 
 
