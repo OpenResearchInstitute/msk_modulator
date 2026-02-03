6c16836fed57849a00c0cd6c060fe9962e90dd28
Matthew Wishek <matthew@wishek.com>
2026-02-03 11:07:05 -0700

    Capture latest updates

--- model/msk_modulator.py
+++ model/msk_modulator.py
@@ -1,13 +1,82 @@
 #!/usr/bin/env python
 
-from msk_modulator import msk_modulator
+import sys
+import os
+import pyqtgraph as pg
+from   pyqtgraph.Qt import QtCore
+import numpy as np
+
+# Add the parent directory (where all submodules live) to Python path
+submodule_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+project_root = os.path.dirname(submodule_root)
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+from msk_modulator   import msk_modulator
+from prbs.model.prbs import prbs
 
 bitrate 	 = 54200
 freq_if_mult = 32
-sample_rate  = 61.44e6
-
+sample_rate  = 5e6 #61.44e6
 
 mod = msk_modulator.modulator(bitrate, freq_if_mult, sample_rate)
+gen = prbs.prbs_generator()
 
+app = pg.mkQApp()
+win = pg.GraphicsLayoutWidget(show=True, title="Realtime Plot")
 
+	# Create two plots
+p1 = win.addPlot(title="I Channel")
+p2 = win.addPlot(title="Q Channel", row=1, col=0)
+p3 = win.addPlot(title="Q Channel", row=2, col=0)
 
+curve1 = p1.plot(pen='r')
+curve2 = p1.plot(pen='b')
+curve3 = p2.plot(pen='g')
+curve4 = p3.plot(pen='y')
+
+sample_buffer = np.zeros(250)
+bit_buffer = np.zeros(250)
+f1_buffer = np.zeros(250)
+f2_buffer = np.zeros(250)
+
+ptr = 0
+
+iq = [0, 0, 1]
+bit = 0
+
+def update():
+	global sample_buffer, bit_buffer, ptr, iq, bit, f1_buffer, f2_buffer
+
+	if iq[2]:
+		bit = gen.bit()
+		print("bit: ", bit)
+		mod.txbit(bit)
+
+	iq  = mod.get_sample()
+	new_sample = iq[0]
+	new_f1s = mod.f1s 
+	new_f2s = mod.f2s
+
+	sample_buffer[:-1] = sample_buffer[1:]
+	sample_buffer[-1]  = new_sample
+	    
+	bit_buffer[:-1] = bit_buffer[1:]
+	bit_buffer[-1]  = bit
+
+	f1_buffer[:-1] = f1_buffer[1:]
+	f1_buffer[-1]  = new_f1s
+
+	f2_buffer[:-1] = f2_buffer[1:]
+	f2_buffer[-1]  = new_f2s
+
+	curve1.setData(sample_buffer)
+	curve2.setData(bit_buffer)
+	curve3.setData(f1_buffer)
+	curve4.setData(f2_buffer)
+
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(50)  # 50ms update rate
+
+app.exec_()
--- model/msk_modulator/msk_modulator.py
+++ model/msk_modulator/msk_modulator.py
@@ -1,92 +1,103 @@
 import numpy as np 
 
-class ClassName(object):
-	"""docstring for ClassName"""
-	def __init__(self, arg):
-		super(ClassName, self).__init__()
-		self.arg = arg
+from nco.model.nco   	import nco
+from prbs.model.prbs	import prbs
 
 class modulator():
 
 		def __init__(self, bitrate, freq_if_mult, sample_rate, seed=1):
 			super(modulator, self).__init__()
 
+			self.bitrate = bitrate
+
 			delta_f = bitrate/4
 			freq_if = delta_f * freq_if_mult
 
 			self.freq_f1 = freq_if - delta_f
 			self.freq_f2 = freq_if + delta_f
+			print("delta_f", delta_f)
+			print("freq_if", freq_if)
+			print("f1: ", self.freq_f1)
+			print("f2: ", self.freq_f2)
 
 			self.sample_rate = sample_rate
 
-			self.nco_f1 = nco(self.freq_f1, self.sample_rate)
-			self.nco_f2 = nco(self.freq_f2, self.sample_rate)
+			self.nco_br = nco.nco(self.bitrate, self.sample_rate)
+			self.nco_f1 = nco.nco(self.freq_f1, self.sample_rate)
+			self.nco_f2 = nco.nco(self.freq_f2, self.sample_rate)
 
-			self.prbs = prbs(seed);
-			self.encoder = encoder();
+			self.encoder  = encoder();
 
 			self.samples_per_bit = sample_rate / bitrate
-			self.sample_counter = 0
+
+			self.f1s = 0
+			self.f2s = 0
+			
 
 		def init(self):
 			self.sample_counter = 0
+			self.nco_f1.init()
+			self.nco_f2.init()
+
+		def txbit(self, bit):
+			self.pos_neg = self.encoder.encode(bit)
+			#print("bit: ", bit, "pos: ", self.pos_neg[0], "neg: ", self.pos_neg[1])
 
 		def get_sample(self):
-			if sample_counter == 0:
-				self.bit = self.prbs.git_bit()
-			return self.bit
+			f1 = self.nco_f1.get_sin_cos()
+			f2 = self.nco_f2.get_sin_cos()
+			br = self.nco_br.get_sin_cos()
+			#print("freq: %f %f" % (f1[0], f2[0]))
 
+			rollover = br[2]
 
-class prbs():
+			self.f1s = f1[0] * self.pos_neg[0]
+			self.f1c = f1[1] * self.pos_neg[0]
 
-	def __init__(self, seed):
-		super(prbs, self).__init__()
-		self.seed = seed
+			self.f2s = f2[0] * self.pos_neg[1]
+			self.f2c = f2[1] * self.pos_neg[1]
 
-	def get_bit(self):
-		return 1
+			b_raw = f1[1] * f2[1] + f1[0] * f2[0] # cos(f1) * sin(f2)
+			self.encoder.b = -1 if b_raw >= 0 else 1  # Convert to ±1
+			print(b_raw, self.encoder.b)
+
+			#print("f1s: %f f1c: %f f2s: %f f2c: %f" % (f1s, f1c, f2s, f2c))
+
+			txss = self.f1s + self.f2s
+			txsc = self.f1c + self.f2c
+
+			#print ("txss: %f txsc: %f" % (txss, txsc))
+
+			return [txss, txsc, rollover]
 
 
 class encoder():
 	"""Differential Encoder for MSK Modem"""
 	def __init__(self):
 		super(encoder, self).__init__()
-		self.x_xor = 0
-		self.b  = 1
+		self.x_xor = 1
+		self.b  = -1
 
 	def init(self):
-		self.x_xor = 0
-		self.b  = 1
+		self.x_xor = 1
+		self.b  = -1
 
 	def encode(self, bit):
+
+		twobit = -1 if bit==1 else 1
+
+		pos_x = (twobit + 1) >> 1
+		neg_y = (twobit - 1) >> 1
 		
-		pos = (bit + 1) >> 1
-		neg = (bit - 1) >> 1
-
-		pos_xor = pos * self.x_xor
-
-		neg_b = neg * self.b
-
+		neg_b   = neg_y * self.b
+		pos_xor = pos_x * self.x_xor
 		neg_xor = neg_b * self.x_xor
 
 
-		self.x_xor = bit * self.x_xor
-		self.b = -1 * self.b
+		print("bit: %2d; twobit: %2d; pos_x: %2d; neg_y: %2d; b: %2d; neg_b: %2d, pos_xor: %2d; neg_xor: %2d; x_xor: %2d" %
+					(bit, twobit, pos_x, neg_y, self.b, neg_b, pos_xor, neg_xor, self.x_xor))
 
-		return pos, neg;
+		self.x_xor = twobit * self.x_xor
+		#self.b 	   =     -1 * self.b
 
-
-class nco():
-	"""NCO"""
-	def __init__(self, freq, sample_rate):
-		super(nco, self).__init__()
-		self.freq = freq
-		self.step = 1/sample_rate
-
-	def init(self):
-		self.step = 0
-
-	def get_sample(self):
-		sample_cos = np.cos(2*np.pi*self.freq+self.step)
-		sample_sin = np.sin(2*np.pi*self.freq+self.step)
-		return cos, sin
+		return [pos_xor, neg_xor];
